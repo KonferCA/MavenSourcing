e037a80f7c424d03d0d1727b6272a17450442aee
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-10-12 20:04:52 -0400

    Proposed card design

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -1,46 +1,41 @@
+import React from 'react'
 import { owlImage, catImage, rocksImage } from '@assets'
-
 import './Features.css'
 
-const Features: React.FC = () => {
-    return (
-        <section className="features-container">
-            <div className="feature">
-                <div className="feature-content">
-                    <h2>Malleable</h2>
-                    <p>
-                        We can provide and delivery services from anywhere, and
-                        of any type, around the world.
-                    </p>
-                </div>
-                <img src={catImage} alt="Cat squeezed into a bookshelf" />
-            </div>
-
-            <div className="feature dark">
-                <img src={rocksImage} alt="Stacked stones balancing" />
-                <div className="feature-content">
-                    <h2>Valiant</h2>
-                    <p>
-                        We have provided successful services to an array clients
-                        - we aren't afraid to take risks. We deal with any sort
-                        of client coupled with any sort of sourcing requirement.
-                    </p>
-                </div>
-            </div>
-
-            <div className="feature">
-                <div className="feature-content">
-                    <h2>Noble</h2>
-                    <p>
-                        We believe in integrity, and being a sly or slick
-                        company is the last thing on our agenda. Take a look at
-                        who we are and what we do.
-                    </p>
-                </div>
-                <img src={owlImage} alt="Owl perched on a post" />
-            </div>
-        </section>
-    )
+interface FeatureProps {
+    title: string;
+    description: string;
+    image: string;
 }
 
-export { Features }
+const Feature: React.FC<FeatureProps> = ({ title, description, image }) => (
+    <div className="feature">
+        <img src={image} alt={title} className="feature-image" />
+        <div className="feature-content">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    </div>
+);
+
+const Features: React.FC = () => (
+    <section className="features-container">
+        <Feature
+            title="Malleable"
+            description="We provide and deliver services from anywhere, of any type, around the world."
+            image={catImage}
+        />
+        <Feature
+            title="Valiant"
+            description="We've successfully served diverse clients, taking calculated risks. We handle any client with any sourcing requirement."
+            image={rocksImage}
+        />
+        <Feature
+            title="Noble"
+            description="Integrity is our core value. We prioritize honesty and transparency in all our dealings."
+            image={owlImage}
+        />
+    </section>
+);
+
+export default Features;
