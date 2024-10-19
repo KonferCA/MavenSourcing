00414170820faa0c2bc3cd6b14d0545ae165ea96
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-10-18 21:13:15 -0400

    Add smooth scroll

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,10 +5,18 @@ const Landing: React.FC = () => {
     return (
         <>
             <Navbar />
-            <Hero />
-            <Features />
-            <Services />
-            <Contact />
+            <div id="maven">
+                <Hero />
+            </div>
+            <div id="about">
+                <Features />
+            </div>
+            <div id="services">
+                <Services />
+            </div>
+            <div id="contact">
+                <Contact />
+            </div>
             <Footer />
         </>
     );
